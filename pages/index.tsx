d8c73ee15b9fa09be513774dd45c8e6df4c239d2
mellolucas <dev@lufom.com>
2022-04-23 16:08:58 -0300

    feat(index): new placeholder on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
-import { useState } from 'react';
-
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <Count />
-    </div>
-  )
-}
-
-function Count() {
-  const [Count, setCount] = useState(0);
-  return (
-    <div>
-      <p>You clicked {Count} times</p>
-      <button onClick={() => setCount(Count + 1)}>
-        Click me
-      </button>
+      <h1>Under Development</h1>
+      <p>
+        <a href="https://lufom.com">
+          <img
+            src="https://i.giphy.com/media/eexxDzCm3TeMGlouQI/giphy.webp"
+            alt="Animated image of a cat coding on a laptop"
+            title="This section is under construction"
+            loading="lazy"
+          />
+        </a>
+      </p>
+      <p>
+        <a href="https://lufom.com">
+          <img
+            src="https://github-readme-stats.vercel.app/api?username=mellolucas&count_private=true&show_icons=true&hide=stars,issues&theme=tokyonight&locale=en"
+            alt="Lucas Mello's GitHub stats card"
+            title="GitHub Stats"
+            loading="lazy"
+          />
+        </a>
+      </p>
     </div>
   )
 }
