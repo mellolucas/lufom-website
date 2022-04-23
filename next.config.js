e26de3014260efc61d05916156fb71e9d5b0e1a7
mellolucas <dev@lufom.com>
2022-04-23 17:46:41 -0300

    feat(index): optimize images for CDN

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['i.giphy.com', 'github-readme-stats.vercel.app'],
+  },
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+
 function Home() {
   return (
     <div>
       <h1>Under Development</h1>
       <p>
-        <a href="https://lufom.com">
-          <img
+          <Image
             src="https://i.giphy.com/media/eexxDzCm3TeMGlouQI/giphy.webp"
             alt="Animated image of a cat coding on a laptop"
-            title="This section is under construction"
-            loading="lazy"
+            width={400}
+            height={400}
           />
-        </a>
       </p>
       <p>
-        <a href="https://lufom.com">
-          <img
+          <Image
             src="https://github-readme-stats.vercel.app/api?username=mellolucas&count_private=true&show_icons=true&hide=stars,issues&theme=tokyonight&locale=en"
             alt="Lucas Mello's GitHub stats card"
-            title="GitHub Stats"
-            loading="lazy"
+            width={600}
+            height={200}
           />
-        </a>
       </p>
     </div>
-  )
+  );
 }
 
 export default Home;
